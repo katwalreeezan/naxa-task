c11d527b1c0194218b0fff330bf45bb182b85457
katwalreezan <chemankatwal111@gmail.com>
2023-02-24 19:22:09 +0545

    added react-scroll

--- src/components/Services.jsx
+++ src/components/Services.jsx
@@ -1,18 +1,18 @@
 import React, { useEffect } from 'react';
-import { useDispatch, useSelector} from 'react-redux';
+import { useDispatch, useSelector } from 'react-redux';
 import { fetchData } from '../redux/actions';
 import './Services.css';
 import ReactHtmlParser from 'react-html-parser';
-
+import { Link } from 'react-scroll';
 
 const Services = () => {
-  
-  const dispatch=useDispatch()
-  useEffect(()=>{
-    dispatch(fetchData())
-  },[dispatch])
-  const data=useSelector((state)=>state.servicesData.data)
-  console.log(data)
+  const dispatch = useDispatch();
+  useEffect(() => {
+    dispatch(fetchData());
+  }, [dispatch]);
+
+  const data = useSelector((state) => state.servicesData.data);
+  console.log(data);
 
   if (data.loading) {
     return <p>Loading...</p>;
@@ -21,29 +21,33 @@ const Services = () => {
   if (data.error) {
     return <p>Error: {data.error}</p>;
   }
-  
-
-  return (<div>
-     <h2> Our Services</h2>
-     <div className='title'>
-     {data.map((title)=><h3>{title.title}</h3>)}
-     </div>
+  return (
+    <div>
+      <h2> Our Services</h2>
+      <div className='title'>
+        {data.map((title) => (
+          <Link to={title.id} smooth={true} duration={500} key={title.id}>
+            <h3>{title.title}</h3>
+          </Link>
+        ))}
+      </div>
       {data.map((service) => (
-          <div key={service.id} className='maincard'>
-            <div className='card'>
-            <img src={service.photo} alt='pic'/>
+        <div key={service.id} className='maincard' id={service.id}>
+          <div className='card'>
+            <img src={service.photo} alt='pic' />
             <h3>{service.title}</h3>
             <div className='card-body'>
-            <p align ='justify'>{ReactHtmlParser(service.description1)}</p>
+              <p>{ReactHtmlParser(service.description1)}</p>
             </div>
             <div className='card-bodytwo'>
-            <p>{ReactHtmlParser(service.description2)} </p>
+              <p>{ReactHtmlParser(service.description2)} </p>
             </div>
+          </div>
+          <img src={service.icon} alt='icon' className='icon' />
+        </div>
+            ))}
             </div>
-            <img src={service.icon} alt='icon' className='icon'/>
-            </div>
-        ))}
-    </div>
- );
-};
-export default Services
+          );
+        };
+        
+        export default Services;
